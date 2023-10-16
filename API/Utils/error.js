ffbd7b71df958feac800d6a3012ee3770705bb56
Guro <70147494+gurokirta@users.noreply.github.com>
2023-10-16 13:45:25 +0400

    Completed Sign Up page UI

--- API/Utils/error.js
+++ API/Utils/error.js
@@ -1,9 +1,7 @@
 export const errorHandler = (statusCode, message) => {
   //! ვქმნით ერორს
   const error = new Error();
-  //! თუ error-ში გვაქვს statusCode მას მნიშნველობას ვუცვლით არგუმენტიდან მიღებული statusCode-ით
   error.statusCode = statusCode;
-  //! თუ error-ში გვაქვს message მას მნიშნველობას ვუცვლით არგუმენტიდან მიღებული message-ით
   error.message = message;
 
   return error;
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,3 +1,45 @@
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold my-7 text-center">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="border rounded-lg p-3 focus:outline-none"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="border rounded-lg p-3 focus:outline-none"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="border rounded-lg p-3 focus:outline-none"
+        />
+
+        <button
+          className="bg-slate-700
+          rounded-lg
+          p-3 uppercase
+        text-white hover:opacity-70 
+          disabled:opacity-90"
+        >
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p className="">Have an Account ?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
