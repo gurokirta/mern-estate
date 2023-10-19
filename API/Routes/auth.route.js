692c984b088c936cc4f48433e80ce564dcbcc6cf
Guro <70147494+gurokirta@users.noreply.github.com>
2023-10-19 14:22:54 +0400

    Created Sign In API route

--- API/Routes/auth.route.js
+++ API/Routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../Controllers/auth.controller.js";
+import { signup, signin } from "../Controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
+authRouter.post("/signin", signin);
 
 export default authRouter;
