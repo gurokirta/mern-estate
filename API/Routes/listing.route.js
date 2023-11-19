fa31690fb19fa1c96a8ee6ab1fabe46385974bd8
Guro <70147494+gurokirta@users.noreply.github.com>
2023-11-19 18:10:11 +0400

    Created listing update API Route

--- API/Routes/listing.route.js
+++ API/Routes/listing.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createListing, deleteListing } from "../Controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+  updateListing,
+} from "../Controllers/listing.controller.js";
 import { verifyToken } from "../Utils/verifyUser.js";
 
 const listingRouter = express.Router();
 
 listingRouter.post("/create", verifyToken, createListing);
 listingRouter.delete("/delete/:id", verifyToken, deleteListing);
+listingRouter.post("/update/:id", verifyToken, updateListing);
 
 export default listingRouter;
