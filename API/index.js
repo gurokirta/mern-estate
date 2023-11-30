cb6c92757f852ea5fc0bb09b925bf70694d74fea
Guro <70147494+gurokirta@users.noreply.github.com>
2023-11-30 16:00:30 +0400

    Updated

--- API/index.js
+++ API/index.js
@@ -22,6 +22,7 @@ const app = express();
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/client/dist")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
