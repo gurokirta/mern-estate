99f6df9629139347796ecb592080753e397be7ae
Guro <70147494+gurokirta@users.noreply.github.com>
2023-12-04 13:07:27 +0400

    Prepared for deploy

--- API/index.js
+++ API/index.js
@@ -5,8 +5,16 @@ import cookieParser from "cookie-parser";
 import userRouter from "./Routes/user.route.js";
 import authRouter from "./Routes/auth.route.js";
 import listingRouter from "./Routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 mongoose
   .connect(process.env.MONGO_DB)
